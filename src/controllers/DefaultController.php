21364b267618bf3bee9fb232bb09cd3c40c27c0d
RadCraftplay <radcraftplay2@gmail.com>
2022-12-10 12:12:02 +0100

    Added simple router

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+    
+    public function index() {
+        // TODO: display index.html
+        die("index method");
+    }
+
+    public function login() {
+        // TODO: display login.html
+        die("login method");
+    }
+}
