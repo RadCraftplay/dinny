fa68e59e5c1848c85cc37f056e921ab25837f186
RadCraftplay <radcraftplay2@gmail.com>
2022-12-10 12:25:22 +0100

    Implemented routing for all views

--- index.php
+++ index.php
@@ -7,4 +7,7 @@ $path = parse_url($path, PHP_URL_PATH);
 
 Router::get('index', 'DefaultController');
 Router::get('login', 'DefaultController');
+Router::get('register', 'DefaultController');
+Router::get('server', 'DefaultController');
+Router::get('submit_server', 'DefaultController');
 Router::run($path);
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -11,4 +11,16 @@ class DefaultController extends AppController {
     public function login() {
         $this->render('login');
     }
+
+    public function register() {
+        $this->render('register');
+    }
+
+    public function server() {
+        $this->render('server');
+    }
+
+    public function submit_server() {
+        $this->render('submit-server');
+    }
 }
