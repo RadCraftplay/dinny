bf3fd4223ea461905d1c917279d53f322a33c74a
RadCraftplay <radcraftplay2@gmail.com>
2022-12-14 17:53:48 +0100

    Fixed error page title

--- public/views/error.php
+++ public/views/error.php
@@ -6,7 +6,7 @@ if (session_status() != PHP_SESSION_ACTIVE){
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Homepage</title>
+    <title>Error</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap">
     <link rel="Stylesheet" type="text/css" href="public/css/base.css"/>
     <link rel="Stylesheet" type="text/css" href="public/css/homepage.css"/>
